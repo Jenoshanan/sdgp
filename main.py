3143c36c64987d20d3507304e8778ff42c154732
Jenoshanan <66421706+Jenoshanan@users.noreply.github.com>
2021-04-19 17:05:59 +0530

    login

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from flask import Flask, render_template, request,session
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
